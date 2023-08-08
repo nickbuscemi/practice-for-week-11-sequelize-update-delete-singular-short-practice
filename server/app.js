20130ae35b7ff3a33d3edebe28f9ad4807ca98b5
Nick <nick.buscemi13@gmail.com>
2023-08-08 15:31:16 -0400

    finished

--- server/app.js
+++ server/app.js
@@ -21,12 +21,47 @@ app.get('/puppies', async (req, res, next) => {
 // STEP 1: Update a puppy by id
 app.put('/puppies/:puppyId', async (req, res, next) => {
     // Your code here
+    const { puppyId } = req.params;
+    const { ageYrs, weightLbs, microchipped } = req.body;
+
+    try {
+        const puppy = await Puppy.findByPk(puppyId);
+        if (!puppy) return res.status(404).json({ message: 'Puppy not found'});
+
+        if (ageYrs !== undefined) puppy.ageYrs = ageYrs;
+        if (weightLbs !== undefined) puppy.weightLbs = weightLbs;
+        if (microchipped !== undefined) puppy.microchipped = microchipped;
+
+        await puppy.save();
+
+        res.json({
+            message: `Successfully updated puppy with id ${puppyId}.`,
+            puppy
+        });
+    } catch (error) {
+        res.status(500).json({ message: 'Internal server error' });
+    }
 })
 
 
 // STEP 2: Delete a puppy by id
 app.delete('/puppies/:puppyId', async (req, res, next) => {
     // Your code here
+    const { puppyId } = req.params;
+
+    try {
+        const puppy = await Puppy.findByPk(puppyId);
+        if (!puppy) return res.status(404).json({ message: 'Puppy not found'});
+
+        await puppy.destroy();
+        
+        res.json({
+            message: `Successfully deleted puppy with id ${puppyId}.`,
+            puppy
+        });
+    } catch (error) {
+        res.status(500).json({ message: 'Internal server error' });
+    }
 })
 
 
